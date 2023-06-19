ffe1f12a611ad41afe36869b8636abd93c7e99b4
Gokul Prathin <gokulprathin8@gmail.com>
2023-06-19 13:40:49 -0400

    base url config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://pygeoweaver-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pygeoweaver-docs.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
