109673a2572b41365908923c20e49d67128509ca
Gokul Asamani <gokulprathin8@gmail.com>
2023-06-19 12:14:58 -0400

    deployment changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/geoweaver-icon.png',
 
   // Set the production url of your site here
-  url: 'https://geobrain.csiss.gmu.edu',
+  url: 'https://pygeoweaver-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
